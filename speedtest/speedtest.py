e847d114b302c83a5398e0544a7255e0efd67eef
Brramble <Brramble@users.noreply.github.com>
2017-02-18 17:44:05 +0000

    Added if statement
    Added if statement

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -3,27 +3,29 @@ from discord.ext import commands
 import subprocess
 import asyncio
 
-class SpeedTest:
+class Speedtest:
 
     def __init__(self,bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
     @commands.cooldown(1, 3600)
-    async def speedtest(self):
+    async def speedtest(self, ctx):
+        server = ctx.message.server
         
-        """Test host speed!"""
+        """Test internet speeds of the bot's host! Coded by Bramble."""
         
-        await self.bot.say(":warning: This may cause disruption to the bot temporarily and may take a while...")
-        speedresult = subprocess.check_output("speedtest-cli --secure --simple", shell=True).decode()
-        message = speedresult
-        em = discord.Embed(colour=0xEC2323, timestamp=__import__('datetime').datetime.utcnow())
-        em.set_thumbnail(url=self.bot.user.avatar_url)
-        em.add_field(name=":gear: SpeedTest Result\n", value="{}".format(speedresult))
-        em.set_footer(text="Cooldown of 3600 seconds initiated")
-        await self.bot.say(embed=em)
-        
+        if self.bot.is_voice_connected(server):
+            await self.bot.say("You must make me leave the voice channel before I do my test.")
+        else:
+            await self.bot.say("Warning: This may cause disruption to the bot temporarily and may take a while...")
+            speedresult = subprocess.check_output("speedtest-cli --secure --simple", shell=True).decode()
+            em = discord.Embed(colour=0xEC2323, timestamp=__import__('datetime').datetime.utcnow())
+            em.set_thumbnail(url=self.bot.user.avatar_url)
+            em.add_field(name=":gear: SpeedTest Result\n", value="{}".format(speedresult))
+            em.set_footer(text="Cooldown of 3600 seconds initiated")
+            await self.bot.say(embed=em)       
 
 def setup(bot):
-    n = SpeedTest(bot)
+    n = Speedtest(bot)
     bot.add_cog(n)
