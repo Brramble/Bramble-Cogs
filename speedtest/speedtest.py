0aaee7d563bf2a51b1a4a101ce3bb7800c6fc5f0
Brramble <Brramble@users.noreply.github.com>
2017-02-18 14:39:13 +0000

    Cooldown update
    Cooldown update

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -12,12 +12,12 @@ class Speedtest:
     @commands.cooldown(1, 3600)
     async def speedtest(self):
         """Test host speed!"""  
-        await self.bot.say(":warning: SPEED TESTING :warning:")
-        await self.bot.say("This may cause disruption to the bot temporarily. This may take a while...") 
+        await self.bot.say(":warning:**SPEED TESTING**\n\nThis may cause disruption to the bot temporarily. This may take a while...")
         speedresult = subprocess.check_output("speedtest-cli --secure --simple", shell=True).decode()
         message = speedresult
         message = '```{}```'.format(speedresult)
         await self.bot.say(message)
+        await self.bot.say("Cooldown of 3600 seconds initiated.")
 
 def setup(bot):
     n = Speedtest(bot)
